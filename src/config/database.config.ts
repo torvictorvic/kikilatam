4ec5b2075e8f95fe25d2c3e9ddf19df47aa4de72
torvictorvic <victormst@gmail.com>
2024-11-08 07:52:27 -0500

    Credenciales

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,10 +2,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || '192.168.1.8',
+  host: process.env.DB_HOST || '',
   port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '123456',
+  username: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'gestion_envios',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
